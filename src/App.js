d70ca9902bf68ba6dea2d5007834771e449521ff
Basil <Vaseokk@gmail.com>
2020-08-12 19:14:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets do a Netlix Clone
         </a>
       </header>
     </div>

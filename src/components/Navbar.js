0001b3bc596ddd626a0fd05fce5e84bb25174eae
Basil <Vaseokk@gmail.com>
2020-08-18 18:18:47 +0300

    added the navbar-dark

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 
 import logo from '../assets/streamy-logo.png';
 import avatar from '../assets/avatar.png';
@@ -6,13 +6,21 @@ import './Navbar.css';
 
 
 function Navbar() {
+  const [show,handleShow] = useState(false);
 
   useEffect(()=> { 
-    
-  })
+     window.addEventListener('scroll', () => {
+      if(window.scrollY > 5) {
+        handleShow(true);
+      } else handleShow(false)
+     });
+      return () => {
+      window.addEventListener('scroll');
+      };
+  },[])
 
   return (
-    <div className="navbar">
+    <div className={`navbar ${show && 'navbar-dark'}`}>
       <img className="navbar-logo" src={logo} alt="logo" />
       <div className='navbar-space' />
       <img className='navbar-avatar' src={avatar} alt="avatar" />
